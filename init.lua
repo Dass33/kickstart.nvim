9f6416aad4ba6573748d04cefae20037bf03a0e1
Dass33 <94531241+Dass33@users.noreply.github.com>
2024-06-18 17:52:46 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -99,6 +99,8 @@ vim.keymap.set('x', '<leader>p', [["_dP]])
 vim.keymap.set({ 'n', 'v' }, '<leader>y', [["+y]])
 vim.keymap.set('n', '<leader>Y', [["+Y]])
 
+vim.keymap.set({ 'n', 'v' }, '<leader>d', [["_d]])
+
 --change backgroung to transparent
 function ColorMyPencils(color)
   color = color or 'rose-pine'
